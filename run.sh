0040d75cfe43b72045b2995c12aedcee1c900d3a
David <dmarquez.estudis@gmail.com>
2021-06-29 17:49:00 +0200

    build and run using source directory

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ OCFPATH="${OCFBASEPATH}${OCFSUBPATH}"
 PLATFORM=`jq --raw-output '.platforms[0]' ${CURPWD}/${PROJNAME}-config.json`
 
 if [ "$PLATFORM" == "esp32" ]; then
-  MY_COMMAND="cd ${OCFPATH}/iotivity-lite/port/${PLATFORM}/"
+  #MY_COMMAND="cd ${OCFPATH}/iotivity-lite/port/${PLATFORM}/"
+  #eval ${MY_COMMAND}
+  MY_COMMAND="cd ${CURPWD}/src"
   eval ${MY_COMMAND}
   MY_COMMAND="idf.py -p /dev/ttyUSB0 flash monitor"
   eval ${MY_COMMAND}
